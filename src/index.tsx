3d6decae32940decce754fdfc34ad91066843e2a
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-01-11 00:10:01 +0300

    set app locale to tr_TR

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,19 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import "moment/locale/tr";
+import locale from "antd/lib/locale/tr_TR";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ConfigProvider locale={locale}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
